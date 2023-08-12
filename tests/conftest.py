8ddf9eb75e42456933bd53c75d288e217b1027b8
Alexandr Lysenko <ovie48@mail.ru>
2023-08-12 13:14:03 +0300

    refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,7 +46,7 @@ def setup_browser(request):
         options=options
     )
     browser.config.driver = driver
-    browser.config.base_url = 'https://demoqa.com/'
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     yield browser
