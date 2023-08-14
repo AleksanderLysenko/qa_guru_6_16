6f64b86b8a00bba06ca6a81916da239359856bd1
Alexandr Lysenko <ovie48@mail.ru>
2023-08-15 01:16:07 +0300

    Revert "initial project"
    This reverts commit 872bbdc084e4c5f18c563b3b3c481476adb9802a.

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,11 +27,9 @@ def load_env():
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    browser_name = request.config.getoption('--browser_name')
-    browser_name = browser_name if browser_name != "" else DEFAULT_BROWSER_NAME
     options = Options()
     selenoid_capabilities = {
-        "browserName": browser_name,
+        "browserName": "chrome",
         "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
