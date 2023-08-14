872bbdc084e4c5f18c563b3b3c481476adb9802a
Alexandr Lysenko <ovie48@mail.ru>
2023-08-15 01:07:15 +0300

    initial project

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,9 +27,11 @@ def load_env():
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    browser_name = request.config.getoption('--browser_name')
+    browser_name = browser_name if browser_name != "" else DEFAULT_BROWSER_NAME
     options = Options()
     selenoid_capabilities = {
-        "browserName": "chrome",
+        "browserName": browser_name,
         "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
